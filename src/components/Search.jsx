04ce51fa4ef501587b39ac2fbd488e9b1b5e198a
Edgar Santos <edgarrps@gmail.com>
2023-03-06 01:26:56 -0400

    Created a Search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,9 @@
-export  default function Search () {
+export default function Search() {
 
-return (
-    <form>
-        <input type='text' className='rounded-l-full'/>
-    </form>
-)
+    return (
+        <form className='flex place-items-center pt-20'>
+                <input type='text' className='rounded-l-full w-6/12' />
+                <button type='submit' className='bg-blue-200 w-[30px] rounded-r-full'>X</button>
+        </form>
+    )
 }
